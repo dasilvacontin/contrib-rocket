d719e7070fa032e3d6dcbb8dd85a162db2dac9e4
David da Silva <dasilvacontin@gmail.com>
2016-03-28 18:20:15 +0200

    Add .travis.yml and lint using shellcheck

--- contrib-rocket.sh
+++ contrib-rocket.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function contrib-rocket {
-  trail=`gh-contribs $@ | spark`
+  trail=$(gh-contribs "$@" | spark)
   echo "${trail}🚀"
 }
 
 function command-exists {
-  command -v $1 &>/dev/null
+  command -v "$1" &>/dev/null
 }
 
 function install-log {
@@ -37,5 +37,5 @@ unset -f command-exists install-log
 # checks if the script is not being sourced
 # http://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
 if [ "$0" = "$BASH_SOURCE" ]; then
-  contrib-rocket $@
+  contrib-rocket "$@"
 fi
